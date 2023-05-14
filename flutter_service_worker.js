cbd9292146d259d38bd58b08c0f8a0e7e26286c8
Abik Vaidhya <abikvaidhya@gmail.com>
2023-05-14 09:37:06 +0545

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -114,8 +114,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "77d77b1985628ef08c52073b6e945eef",
-"/": "77d77b1985628ef08c52073b6e945eef",
+"index.html": "7c78cda80ea2ec8cd4ac790e2a997ac6",
+"/": "7c78cda80ea2ec8cd4ac790e2a997ac6",
 "main.dart.js": "f179ad4b8ae107662677efa123af9b67",
 "manifest.json": "1082c3f1e96467550009ffb5e6654eb4",
 "version.json": "d351ae99551ba008177d734c54348c46"
